f6aa94ef28f934c26b1443ab5f2636b0d1d7de0f
Pablo Villicaña <dsapablojvn@gmail.com>
2020-09-10 21:07:35 -0500

    Proyecto V01 PDF - C#

---
+++ LectorPDF_V01_dsa/LectorPDF_V01_dsa/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace LectorPDF_V01_dsa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgv2 = new System.Windows.Forms.DataGridView();
+            this.lblMsjs = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1354, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "Read PDF";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "Exit";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FloralWhite;
+            this.panel1.Controls.Add(this.dgv2);
+            this.panel1.Location = new System.Drawing.Point(12, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(709, 624);
+            this.panel1.TabIndex = 1;
+            this.panel1.Visible = false;
+            // 
+            // dgv2
+            // 
+            this.dgv2.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dgv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv2.Location = new System.Drawing.Point(12, 15);
+            this.dgv2.Name = "dgv2";
+            this.dgv2.Size = new System.Drawing.Size(688, 499);
+            this.dgv2.TabIndex = 1;
+            // 
+            // lblMsjs
+            // 
+            this.lblMsjs.AutoSize = true;
+            this.lblMsjs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsjs.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblMsjs.Location = new System.Drawing.Point(12, 34);
+            this.lblMsjs.Name = "lblMsjs";
+            this.lblMsjs.Size = new System.Drawing.Size(0, 16);
+            this.lblMsjs.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1354, 701);
+            this.Controls.Add(this.lblMsjs);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.DataGridView dgv2;
+        private System.Windows.Forms.Label lblMsjs;
+    }
+}
+
---
+++ LectorPDF_V01_dsa/LectorPDF_V01_dsa/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.Text.RegularExpressions;
+
+namespace LectorPDF_V01_dsa
+{
+    public partial class Form1 : Form
+    {
+        //Variables para guardar el UUID Y el MX.
+        string _uuid, _mx;
+        //Variable que guardara la ruta.
+        string ruta;
+        //Creacion de la tabla.
+        DataTable table = new DataTable("DatosFactura");
+        //creacion de una columna.
+        DataColumn column;
+        //creacion de una fila.
+        DataRow row;
+        //Esta expresion analiza un UUID conformado por ceros. 
+        string exp = "^[0]{8}-[0]{4}-[0]{4}-[0]{4}-[0]{12}$";
+        //Esta expresion analiza un UUID valido. 
+        string exp_uuid = "^[A-Z|0-9]{8}-[A-Z|0-9]{4}-[A-Z|0-9]{4}-[A-Z|0-9]{4}-[A-Z|0-9]{12}$";
+        //Esta expresion analiza un MX valido.
+        string exp_mx = "^MX ?[0-9]{6}$";
+        //Esta variable guarda el resultado de la expresion si es que se encuentra una coincidencia.
+        string a;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Inicializo los dos string.
+            _uuid = "uuid";
+            _mx = "-mx-";
+        }
+
+        //Creo la tabla donde guardare los datos. 
+        public void createDataTable()
+        {
+            //Creo la columna para UUID
+            column = new DataColumn();
+            column.DataType = Type.GetType("System.String");
+            column.ColumnName = "UUID";
+            table.Columns.Add(column);
+
+            //Creo la columna para MX
+            column = new DataColumn();
+            column.DataType = Type.GetType("System.String");
+            column.ColumnName = "MX";
+            table.Columns.Add(column);
+        }
+
+        //Este metodo lee el PDF
+        public string ReadPdfFile(object Filename)
+        {
+            string strText = string.Empty;
+            //try para obtener el error en caso de que ocurra
+            try
+            {
+                PdfReader readerPdf = new PdfReader((string)Filename);
+
+                for (int page = 1; page <= readerPdf.NumberOfPages; page++)
+                {
+                    ITextExtractionStrategy its = new iTextSharp.text.pdf.parser.SimpleTextExtractionStrategy();
+                    PdfReader reader = new PdfReader((string)Filename);
+                    String s = PdfTextExtractor.GetTextFromPage(reader, page, its);
+
+                    s = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(s)));
+                    strText = strText + s;
+                    reader.Close();
+                }
+            }
+            //Manejo de errores
+            catch (Exception ex)
+            {
+                //Muestro el error en caso de que ocurra
+                lblMsjs.ForeColor = Color.Crimson;
+                lblMsjs.Text = ex.Message.ToString();
+            }
+            //retorno el texto 
+            return strText;
+        }
+
+        //Este metodo recibe un texto y tambien la expresion que buscara dentro del texto.
+        public string MatchesExp(string texto, string exp)
+        {
+            MatchCollection find = Regex.Matches(texto, exp);
+            //buscara por cada linea la expresion buscada.
+            foreach (Match e in find)
+            {
+                a = e.ToString();
+            }
+            //Devuelve el texto que coicidio 
+            return a;
+        }
+
+        //Boton para leer el PDF.
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            //Este label muestra mensajes en la pantalla, aqui limpio el texto por si tiene.
+            lblMsjs.Text = string.Empty;
+            //Limpio la tabla para que no muestre datos de ls anteriores
+            table.Clear();
+            //Limpio del origen de datos del grid
+            dgv2.DataSource = null;
+            //Esta variable recibe el texto que se obtuvo del PDF. 
+            string salida;
+
+            //Abro una ventana nueva del explorador de y para seleccionar ahivos.
+            OpenFileDialog openArc = new OpenFileDialog();
+            //Le indico que buscara solo archivos de tio 
+            openArc.Filter = "PDF Files | *.pdf";
+            //Obtiene o establece un valor que indica si el cuadro de diálogo muestra una advertencia cuando el usuario 
+            //especifica un nombre de archivo que no existe.
+            openArc.CheckFileExists = true;
+            //Obtiene o establece un valor que indica si el cuadro de diálogo permite seleccionar varios archivos.
+            openArc.Multiselect = true;
+            //Si el usuario da clic en aceptar 
+            if (openArc.ShowDialog() == DialogResult.OK)
+            {
+                //Se muestra un panel con el grid
+                panel1.Visible = true;
+                //Recorro todos los
+                for (int i = 0; i < openArc.SafeFileNames.Count(); i++)
+                {
+                    //Creo una variable que guarde los caracteres que busco dentro del nombre y ruta de los arhivos.
+                    string b = "\\";
+                    //Busco dentro del nombre del archivo la poosicion que me indique la ultima aparicion de la variable b y la guardo en una variable int.
+                    int position = openArc.FileName.LastIndexOf(b);
+                    //Aqui inicializo la variable con la ruta del archivo pero elimino el nombre del archivo.
+                    ruta = @"" + openArc.FileName.Substring(0, position);
+                    //Guardo el texto que me devuelve el metodo de Leer un PDF.
+                    salida = ReadPdfFile(ruta + b + openArc.SafeFileNames[i]);
+                    //Guardo el texto que esta en la variable salida pero al mismo tiempo lo separa por saltos de linea y cada linea es un elemento distinto dentro del arregle.
+                    string[] arregloString = salida.Split('\n');
+                    //Inicializo una nueva fila para la tabla
+                    row = table.NewRow();
+                    //Cada elemento del arreglo sera evaluado con la expresion del UUID y si la expresión existe sera almacenada en una variable.
+                    foreach (string item in arregloString)
+                    {
+                        _uuid = MatchesExp(item, exp_uuid);
+                    }
+                    //Cada elemento del arreglo sera evaluado con la expresion del MX y si la expresión existe sera almacenada en una variable.
+                    foreach (string item in arregloString)
+                    {
+                        _mx = MatchesExp(item, exp_mx);
+                    }
+                    //Agrego las variables si es que se enctontro alguna coincidencia.
+                    row["UUID"] = _uuid;
+                    row["MX"] = _mx;
+                    //Agrego valores a las columnas de las fila.
+                    table.Rows.Add(row);
+                    //Le mando la tabla con datos al gridView.
+                    dgv2.DataSource = table;
+                    //Agrego un mesaje al fnalizar.
+                    lblMsjs.ForeColor = Color.Blue;
+                    lblMsjs.Text = "Success";
+
+                }
+            }
+        }
+
+        //Boton para salir de la aplicación.
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Aqui inicializo la tabla.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            createDataTable();
+
+        }
+    }
+}
